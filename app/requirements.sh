fe20e7c5b97fbf4b3e8c3bddcf1000e9bef6cbbc
Louie Hernandez <louieghernandez95@gmail.com>
2022-12-05 23:38:49 -0600

    update requirements sh

---
+++ app/requirements.sh
@@ -0,0 +1,6 @@
+pip install diffusers
+pip install accelerate
+pip install gunicorn
+pip install uvircorn
+pip install transformers
+pip install flask-cors
--- app/script.js
+++ app/script.js
@@ -2,6 +2,7 @@ let loader;
 let submitButton;
 let searchBar;
 let results;
+const test = false;
 
 document.addEventListener("DOMContentLoaded", function () {
     loader = document.getElementById("loading-screen");
@@ -34,10 +35,14 @@ async function textToImage() {
     submitButton.disabled = true;
     searchBar.disabled = true;
 
-    let res = await fetch(`http://${server_addr}:${server_port}/image?` +
+	const url = test ? `http://${server_addr}:${server_port}`: 
+        'https://notebooksa.jarvislabs.ai/fd4NNqsipP_wrGj5q1wb0U0rNdMSjq2hQR46e4Jx9jc68BhSnkvBV8ofx1ZPQMMs'
+    
+    let res = await fetch(`${url}/image?` +
         new URLSearchParams({ q: inputValue }),
         {
-            method: 'GET'
+            method: 'GET',
+            keepalive: true
         });
   
     let responseText = '';
@@ -59,4 +64,17 @@ async function textToImage() {
     searchBar.disabled = false;
 
     console.log(responseText);
+}
+
+async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 8000 } = options;
+    
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
+    });
+    clearTimeout(id);
+    return response;
 }
