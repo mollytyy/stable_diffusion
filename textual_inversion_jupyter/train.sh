0569803f6cee8e0e0544237bd61acd061c722248
louiehernandez95 <louieghernandez95@gmail.com>
2022-12-07 20:43:37 -0600

    upload jupyter notebook

---
+++ textual_inversion_jupyter/train.sh
@@ -0,0 +1,16 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export DATA_DIR="images/"
+
+python textual_inversion.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$DATA_DIR \
+  --learnable_property="object" \
+  --placeholder_token="<electron>" --initializer_token="electronics" \
+  --resolution=256 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=2000 \
+  --learning_rate=5.0e-04 --scale_lr \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --output_dir="textual_inversion_elec"
